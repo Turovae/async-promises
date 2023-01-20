3752df70ebefd323d326cfadb849004596383abc
Turovae <ekaterina.turova@mail.ru>
2023-01-20 23:27:35 +0700

    change load method

--- src/modules/GameSavingLoader.js
+++ src/modules/GameSavingLoader.js
@@ -4,9 +4,7 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static load() {
-    return new Promise((resolve) => {
-      resolve(read());
-    })
+    return read()
       .then((data) => json(data))
       .then((data) => new GameSaving(data));
   }
